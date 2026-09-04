6c8b6e4635010e05e7e02abf00f8528974cecdab
Sahil Kumar Sahu <sahil.sahu4102@gmail.com>
2026-09-05 00:23:14 +0530

    Time: 15 ms (90.16%), Space: 108.8 MB (17.32%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int i : nums){
+            if(!set.add(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
