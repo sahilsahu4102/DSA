de3192105f21722f44f300a647336aa2f2b07c11
Sahil Kumar Sahu <sahil.sahu4102@gmail.com>
2026-09-05 01:05:29 +0530

    Time: 2 ms (91.79%), Space: 47.7 MB (57.5%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,13 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int l=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-            int temp=nums[i];
-            nums[i]=nums[l];
-            nums[l]=temp;
-            l++;}
+        int l = 0;
+        for (int r = 0; r < nums.length; r++) {
+            if (nums[r] != 0) {
+                int temp = nums[r];
+                nums[r] = nums[l];
+                nums[l] = temp;
+                l++;
+            }
         }
-        System.out.print(nums);
     }
 }
